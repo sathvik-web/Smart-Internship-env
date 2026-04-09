192c33c417048571c35c4530b7400bf9185fc0a8
Sathvik Dantu <sathvikdantu45@gmail.com>
2026-04-09 21:41:49 +0530

    fixed

--- env/__init__.py
+++ env/__init__.py
@@ -1,5 +1,9 @@
-from env.environment import InternshipEnv
-from env.models import Action, Observation, Reward
-from .graders import easy_grader, medium_grader, hard_grader
-__all__ = ["InternshipEnv", "Observation", "Action", "Reward"]
+# env/__init__.py
+from env.graders import easy_grader, medium_grader, hard_grader
 
+__all__ = ["easy_grader", "medium_grader", "hard_grader"]
+
+def _lazy_load():
+    from env.environment import InternshipEnv
+    from env.models import Action, Observation, Reward
+    return InternshipEnv, Action, Observation, Reward
